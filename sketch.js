7228cdf44dd45176d2212651975124f2e5546bde
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-09-09 16:41:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,19 +63,7 @@ function draw() {
   text("EPIC ARCHERY", width / 2, 100);
 }
 
-function keyPressed() {
-  if (keyCode === 32) {
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle;
-    //console.log(angle);
 
-    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
-  }
-}
 
 function keyReleased() {
   if (keyCode === 32) {
